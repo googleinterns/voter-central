90c69de4fafabb7e6328b2069be6c0a59507bab7
tjyoung99 <ty12101999@gmail.com>
2020-08-03 14:13:26 +0000

    forgot to unstash

--- project/src/main/java/com/google/sps/servlets/PollingLocationsServlet.java
+++ project/src/main/java/com/google/sps/servlets/PollingLocationsServlet.java
@@ -1,3 +1,7 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 private static InfoCompiler infoCompiler = new InfoCompiler();
 private final static String VOTER_INFO_QUERY_URL =
     String.format("https://www.googleapis.com/civicinfo/v2/voterinfo?key=%s",
@@ -10,9 +14,11 @@ public class PollingLocationServlet extends HttpServlet {
     
     String address = request.getParameter("Address");
     String queryUrl = String.format("%s&address=%s", VOTER_INFO_QUERY_URL, address);
-    JsonArray pollingLocationData = infoCompiler.queryCivicInformation(queryUrl).getAsJsonArray("pollingLocations");
+    JsonArray pollingLocationsArray = infoCompiler.queryCivicInformation(queryUrl).getAsJsonArray("pollingLocations");
+    JsonObject Address pollingLocationsArray.get(0).getAsJsonObject();
+    pollingLocation
     Gson gson = new Gson();
-    String pollingLocation = gson.toJson(pollingLocationData);
+    String pollingLocation = gson.toJson(Address);
 
     // Send data in JSON format as the servlet response for the polling location page.
     response.setContentType("application/json;");
