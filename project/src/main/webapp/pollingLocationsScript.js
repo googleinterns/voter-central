fc49554b71d4c57f5db6ae5280ced81bda9b7ae3
tjyoung99 <ty12101999@gmail.com>
2020-08-07 21:16:20 +0000

    Resolving comments

--- project/src/main/webapp/pollingLocationsScript.js
+++ project/src/main/webapp/pollingLocationsScript.js
@@ -18,7 +18,8 @@
 // Returns Address for Polling location from polling location servlet.
 function listPollingLocations() {
   const userAddress = document.getElementById("polling-location-input").elements[0].value;
-  fetch(`/pollingLocation?address=${encodeURIComponent(userAddress).replace(/ /g, "%20")}`).then(response => response.json()).then((pollingAddress) => {
+  fetch(`/pollingLocation?address=${encodeURIComponent(userAddress).replace(/ /g, "%20")}`)
+      .then(response => response.json()).then((pollingAddress) => {
     const pollingAddressElement = document.getElementById('poll-address');
     for (const fields in pollingAddress.address){
       pollingAddressElement.innerHTML += pollingAddress.address[fields] + "<br>";
